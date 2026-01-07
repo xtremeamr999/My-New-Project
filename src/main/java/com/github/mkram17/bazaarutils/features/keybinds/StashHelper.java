1c28aaeeb50cb2fb476935633b68688e90e5363d
mkram17 <1maxkramer@gmail.com>
2026-01-06 20:48:46 -0500

    register keybind

--- src/main/java/com/github/mkram17/bazaarutils/features/keybinds/StashHelper.java
+++ src/main/java/com/github/mkram17/bazaarutils/features/keybinds/StashHelper.java
@@ -1,5 +1,9 @@
 package com.github.mkram17.bazaarutils.features.keybinds;
 
+//? if > 1.21.8 {
+import com.github.mkram17.bazaarutils.BazaarUtils;
+import net.minecraft.util.Identifier;
+//?}
 import com.github.mkram17.bazaarutils.misc.autoregistration.RunOnInit;
 import com.github.mkram17.bazaarutils.utils.GUIUtils;
 import com.github.mkram17.bazaarutils.utils.PlayerActionUtil;
@@ -14,21 +18,20 @@ public class StashHelper {
     @Getter
     private static int ticksBetweenPresses;
     //? if > 1.21.8
-    /*private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of(BazaarUtils.MODID));*/
-
+    private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of(BazaarUtils.MODID));
     private static final KeyBinding keyBinding = new KeyBinding(
        "Pick Up Stash",
        InputUtil.Type.KEYSYM,
        GLFW.GLFW_KEY_V,
        //? if > 1.21.8 {
-       /*CATEGORY
-       *///?} else {
-     "Bazaar Utils"
-        //?}
+       CATEGORY
+       //?} else {
+     /*"Bazaar Utils"
+        *///?}
     );
 
     @RunOnInit
-    public static void registerKeyBinding(){
+    public static void initializeKeybind(){
         KeyBindingHelper.registerKeyBinding(keyBinding);
     }
 
