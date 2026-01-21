45a874e60f63e1d1091ee9111edb740a03fdfa62
mkram17 <1maxkramer@gmail.com>
2026-01-21 15:56:01 -0500

    make NumTextBox work on 1.21.10 (untested), and remove older mc version support for v1.0.0 branch

--- src/main/java/com/github/mkram17/bazaarutils/ui/NumTextBox.java
+++ src/main/java/com/github/mkram17/bazaarutils/ui/NumTextBox.java
@@ -2,28 +2,23 @@ package com.github.mkram17.bazaarutils.ui;
 
 import io.wispforest.owo.ui.component.TextBoxComponent;
 import io.wispforest.owo.ui.core.Sizing;
+import net.minecraft.client.input.KeyInput;
+import org.lwjgl.glfw.GLFW;
 
 public class NumTextBox extends TextBoxComponent {
     public NumTextBox(Sizing horizontalSizing) {
         super(horizontalSizing);
+        // Use a text predicate to enforce that only digits can be entered locally.
+        this.setTextPredicate(text -> text.matches("\\d*"));
     }
 
     @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if(!Character.isDigit(keyCode) && keyCode != 259) { // 259 = backspace
+    public boolean keyPressed(KeyInput input) {
+        int keyCode = input.key();
+        if (!Character.isDigit(keyCode) && keyCode != GLFW.GLFW_KEY_BACKSPACE) {
             return false;
         } else {
-            return super.keyPressed(keyCode, scanCode, modifiers);
-        }
-    }
-
-    @Override
-    public boolean charTyped(char chr, int modifiers) {
-        if(!Character.isDigit(chr)) {
-            return false;
-        } else {
-            return super.charTyped(chr, modifiers);
+            return super.keyPressed(input);
         }
     }
 }
-
