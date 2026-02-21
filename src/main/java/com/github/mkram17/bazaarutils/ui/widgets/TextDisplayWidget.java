a1d245237b207daa01abc8ae20699c21242b714d
0xar-ds <sadden-rope-sleek@duck.com>
2026-02-21 01:34:20 -0300

    Add Alignment argument to TextDisplayWidget
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/ui/widgets/TextDisplayWidget.java
+++ src/main/java/com/github/mkram17/bazaarutils/ui/widgets/TextDisplayWidget.java
@@ -7,25 +7,41 @@ import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 
-public class TextDisplayWidget extends ClickableWidget {
-    private final Text text;
 
-    public TextDisplayWidget(int x, int y, int width, int height, Text text) {
+public class TextDisplayWidget extends ClickableWidget {
+    public enum Alignment {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    private final Text text;
+    private final Alignment alignment;
+
+    public TextDisplayWidget(int x, int y, int width, int height, Text text, Alignment alignment) {
         super(x, y, width, height, text);
         this.text = text;
+        this.alignment = alignment;
+    }
+
+    public TextDisplayWidget(int x, int y, int width, int height, Text text) {
+        this(x, y, width, height, text, Alignment.LEFT);
     }
 
     @Override
     public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-        int textWidth = textRenderer.getWidth(text);
-        int textX = this.getX() + (this.width - textWidth) / 2;
+
         int textY = this.getY() + (this.height - textRenderer.fontHeight) / 2;
+        int textX = switch (alignment) {
+            case LEFT   -> this.getX();
+            case CENTER -> this.getX() + (this.width - textRenderer.getWidth(text)) / 2;
+            case RIGHT  -> this.getX() + this.width - textRenderer.getWidth(text);
+        };
+
         context.drawText(textRenderer, text, textX, textY, 0xFFFFFFFF, false);
     }
 
     @Override
-    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
-
-    }
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {}
 }
