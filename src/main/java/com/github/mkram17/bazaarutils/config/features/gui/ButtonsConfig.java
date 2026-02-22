6ec6692ff423066b1ffe7a0bdb877ede25196ed2
0xar-ds <sadden-rope-sleek@duck.com>
2026-02-22 07:59:07 -0300

    Refactor OpenOrders & OpenSettings button to @Module-like behaviour
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/config/features/gui/ButtonsConfig.java
+++ src/main/java/com/github/mkram17/bazaarutils/config/features/gui/ButtonsConfig.java
@@ -1,14 +1,14 @@
 package com.github.mkram17.bazaarutils.config.features.gui;
 
-import com.github.mkram17.bazaarutils.features.gui.buttons.BazaarOpenOrdersButton;
-import com.github.mkram17.bazaarutils.features.gui.buttons.BazaarSettingsButton;
-import com.github.mkram17.bazaarutils.features.gui.buttons.Bookmarks;
-import com.github.mkram17.bazaarutils.features.gui.buttons.CancelOrderAndSearch;
 import com.teamresourceful.resourcefulconfig.api.annotations.*;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import lombok.Getter;
 
-@Category(value = "buttons_config")
+@Category(
+        value = "buttons_config",
+        categories = {
+                ButtonsConfig.BookmarksConfig.class
+        }
+)
 @ConfigInfo(
         title = "Buttons Config",
         titleTranslation = "bazaarutils.config.buttons.category.value",
@@ -17,23 +17,15 @@ import net.minecraft.item.Items;
         icon = "pointer"
 )
 public final class ButtonsConfig {
-    @ConfigEntry(id = "general_separator")
-    @ConfigOption.Hidden
-    @ConfigOption.Separator(
-            value = "bazaarutils.config.buttons.separator.general.value",
-            description = "bazaarutils.config.buttons.separator.general.description"
-    )
-    public static boolean GENERAL_BUTTONS_SEPARATOR = true;
-
     @ConfigEntry(
-            id = "mod_settings",
-            translation = "bazaarutils.config.buttons.mod_settings.value"
+            id = "open_settings",
+            translation = "bazaarutils.config.buttons.open_settings.value"
     )
     @Comment(
             value = "Adds a button to selected menus/screen to quick access the mods' settings.",
-            translation = "bazaarutils.config.buttons.mod_settings.description"
+            translation = "bazaarutils.config.buttons.open_settings.description"
     )
-    public static final BazaarSettingsButton MOD_SETTINGS = new BazaarSettingsButton(true);
+    public static final WidgetButton OPEN_SETTINGS_BUTTON = new WidgetButton(true);
 
     @ConfigEntry(
             id = "open_orders",
@@ -47,15 +39,15 @@ public final class ButtonsConfig {
             """,
             translation = "bazaarutils.config.buttons.open_orders.description"
     )
-    public static final BazaarOpenOrdersButton OPEN_ORDERS = new BazaarOpenOrdersButton(true);
+    public static final WidgetButton OPEN_ORDERS_BUTTON = new WidgetButton(true);
 
-    @ConfigEntry(id = "market_separator")
+    @ConfigEntry(id = "container_buttons_separator")
     @ConfigOption.Hidden
     @ConfigOption.Separator(
-            value = "bazaarutils.config.buttons.separator.market.value",
-            description = "bazaarutils.config.buttons.separator.market.description"
+            value = "bazaarutils.config.buttons.separator.container_buttons.value",
+            description = "bazaarutils.config.buttons.separator.container_buttons.description"
     )
-    public static boolean MARKET_BUTTONS_SEPARATOR = true;
+    public static boolean CONTAINER_BUTTONS_SEPARATOR = true;
 
     @ConfigEntry(
             id = "cancel_order_and_search",
@@ -65,15 +57,102 @@ public final class ButtonsConfig {
             value = "Adds a button to an unfilled orders' (or offer) settings page to cancel it and search once again the item.",
             translation = "bazaarutils.config.buttons.cancel_order_and_search.description"
     )
-    public static final CancelOrderAndSearch CANCEL_ORDER_AND_SEARCH = new CancelOrderAndSearch(false, 25, Items.BLUE_TERRACOTTA.toString());
+    public static final SmallContainerButton CANCEL_ORDER_AND_SEARCH = new SmallContainerButton(false, 25);
 
-    @ConfigEntry(
-            id = "bookmarks",
-            translation = "bazaarutils.config.buttons.bookmarks.value"
+    @Category(value = "bookmarks")
+    @ConfigInfo(
+            title = "Bookmarks",
+            titleTranslation = "bazaarutils.config.buttons.bookmarks.category.value",
+            icon = "bookmark"
     )
-    @Comment(
-            value = "Adds a button to an item's page to toggle a quick access button to search the same item on the Bazaar.",
-            translation = "bazaarutils.config.buttons.bookmarks.description"
-    )
-    public static final Bookmarks BOOKMARKS = new Bookmarks(true,0, ItemStack.EMPTY, 18, 4);
+    public static final class BookmarksConfig {
+        @ConfigEntry(id = "introductory_separator")
+        @ConfigOption.Hidden
+        @ConfigOption.Separator(
+                value = "bazaarutils.config.buttons.bookmarks.separator.introductory.value",
+                description = "bazaarutils.config.buttons.bookmarks.separator.introductory.description"
+        )
+        public static boolean BOOKMARKS_INTRODUCTORY_SEPARATOR = true;
+
+        @ConfigEntry(
+                id = "open_bookmark",
+                translation = "bazaarutils.config.buttons.bookmarks.open_bookmark.value"
+        )
+        @Comment(
+                value = "Configures the button that appears on selected menus/screen to quick search the relevant bookmark.",
+                translation = "bazaarutils.config.buttons.bookmarks.open_bookmark.description"
+        )
+        public static final WidgetButton OPEN_BOOKMARK_BUTTON = new WidgetButton(true);
+
+        @ConfigEntry(
+                id = "toggle_bookmark",
+                translation = "bazaarutils.config.buttons.bookmarks.toggle_bookmark.value"
+        )
+        @Comment(
+                value = "Adds a button to every item's page to toggle a quick access button to search the same item on the Bazaar.",
+                translation = "bazaarutils.config.buttons.bookmarks.toggle_bookmark.description"
+        )
+        public static final SmallContainerButton TOGGLE_BOOKMARK_BUTTON = new SmallContainerButton(true, 0);
+    }
+
+    @ConfigObject
+    public static final class WidgetButton {
+        @Getter
+        @ConfigEntry(
+                id = "enabled",
+                translation = "bazaarutils.config.buttons.button:widget.enabled.value"
+        )
+        @Comment(
+                value = "Whether the button will be registered or not",
+                translation = "bazaarutils.config.buttons.button:widget.enabled.description"
+        )
+        public boolean enabled;
+
+        @ConfigEntry(
+                id = "size",
+                translation = "bazaarutils.config.buttons.button:widget.size.value"
+        )
+        public int size = 18;
+
+        @ConfigEntry(
+                id = "spacing",
+                translation = "bazaarutils.config.buttons.button:widget.spacing.value"
+        )
+        public int spacing = 4;
+
+        public WidgetButton(boolean enabled) {
+            this.enabled = enabled;
+        }
+    }
+
+    @ConfigObject
+    public static final class SmallContainerButton {
+        @Getter
+        @ConfigEntry(
+                id = "enabled",
+                translation = "bazaarutils.config.buttons.button:container.enabled.value"
+        )
+        @Comment(
+                value = "Whether the button will be registered or not",
+                translation = "bazaarutils.config.buttons.button:container.enabled.description"
+        )
+        public boolean enabled;
+
+        @Getter
+        @ConfigEntry(
+                id = "slot_number",
+                translation = "bazaarutils.config.buttons.button:container.slot_number.value"
+        )
+        @Comment(
+                value = "The container slot where the button will be registered at",
+                translation = "bazaarutils.config.buttons.button:container.slot_number.description"
+        )
+        @ConfigOption.Range(min = 0, max = 35)
+        public int slotNumber;
+
+        public SmallContainerButton(boolean enabled, int slotNumber) {
+            this.enabled = enabled;
+            this.slotNumber = slotNumber;
+        }
+    }
 }
